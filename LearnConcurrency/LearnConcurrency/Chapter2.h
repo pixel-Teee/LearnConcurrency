d66aaf40edda11e2b9060ba6318d4f1c9c965446
pixel-Teee <1173769234@qq.com>
2022-03-18 21:40:34 +0800

    complete chapter 3

--- LearnConcurrency/LearnConcurrency/Chapter2.h
+++ LearnConcurrency/LearnConcurrency/Chapter2.h
@@ -6,6 +6,8 @@
 #include <numeric>
 #include <functional>
 #include <algorithm>
+#include <mutex>
+#include <map>
 
 namespace Note2dot8 {
 	template<typename Iterator, typename T>
@@ -90,6 +92,147 @@ namespace Note3do2dot6 {
 	//		std::unique_lock<std::mutex> lock_b(rhs.m, std::defer_lock);//leaves mutexes unlocked
 	//		std::lock(lock_a, lock_b);//mutexes are locked here
 	//		swap(lhs.some_detail, rhs.some_detail);
+	//	}
+	//};
+}
+
+namespace Note3dot10
+{
+	class Y
+	{
+		private:
+			int some_detail;
+			mutable std::mutex m;
+
+			int get_detail() const
+			{
+				std::lock_guard<std::mutex> lock_a(m);
+				return some_detail;
+			}
+		public:
+			Y(int sd) : some_detail(sd){}
+
+			friend bool operator==(Y const& lhs, Y const& rhs)
+			{
+				if(&lhs == &rhs)
+					return true;
+				int const lhs_value = lhs.get_detail();
+				int const rhs_value = rhs.get_detail();
+				return lhs_value == rhs_value;
+			}
+	};
+}
+
+namespace Note3dot11 {
+	//class some_resource;
+	//std::shared_ptr<some_resource> resoure_ptr;
+
+	//void foo()
+	//{
+	//	if (!resoure_ptr)
+	//	{
+	//		resoure_ptr.reset(new some_resource);//there
+	//	}
+	//	resoure_ptr->do_something();
+	//}
+
+	class some_resource;
+	std::shared_ptr<some_resource> resource_ptr;
+	std::mutex resource_mutex;
+	//void foo()
+	//{
+	//	std::unique_lock<std::mutex> lk(resource_mutex);//all threads are serailizaed here
+	//	if (!resource_ptr)
+	//	{
+	//		resource_ptr.reset(new some_resource);//only the initialization needs protection
+	//	}
+	//	lk.unlock();
+	//	resource_ptr->do_something();
+	//}
+
+	
+	//void undefined_behaviour_with_double_checked_locking()
+	//{
+	//	if (!resource_ptr)//1
+	//	{
+	//		std::lock_guard<std::mutex> lk(resource_mutex);
+	//		if (!resource_ptr)//2
+	//		{
+	//			resource_ptr.reset(new some_resource);//3
+	//		}
+	//	}
+	//	resource_ptr->do_something();//4
+	//}
+
+	//std::shared_ptr<some_resource> resource_ptr;
+	//std::once_flag resource_flag;//1
+
+	//void init_resource()
+	//{
+	//	resource_ptr.reset(new some_resource);
+	//}
+
+	//void foo()
+	//{
+	//	std::call_once(resource_flag, init_resource);//initialization is called exactly once
+	//	resource_ptr->do_something();
+	//}
+}
+
+namespace Note3dot12 {
+	//class connection_info;
+	//class connection_handle;
+	//class X
+	//{
+	//	private:
+	//		connection_info connection_details;
+	//		connection_handle connection;
+
+	//		std::once_flag connection_init_flag;
+
+	//		void open_connection()
+	//		{
+	//			connection = connection_manager.open(connection_details);
+	//		}
+	//	public:
+	//		X(connection_info const& connection_details_) :
+	//			connection_details_(connection_details_) {
+
+	//		}
+	//		void send_data(data_packet const& data)//1
+	//		{
+	//			std::call_once(connection_init_flag, &X::open_connection, this);//2
+	//			connection.send_data(data);
+	//		}
+	//		data_packet receive_data()//3
+	//		{
+	//			std::call_once(connection_init_flag, &X::open_connection, this);//2
+	//			return connection.receive_data();
+	//		}
+	//};
+}
+
+namespace Note3dot12 {
+	//class dns_entry;
+
+	//class dns_cache
+	//{
+	//	std::map<std::string, dns_entry> entries;
+	//	mutable boost::shared_mutex entry_mutex;
+
+	//public:
+	//	dns_entry find_entry(std::string const& domain) const
+	//	{
+	//		boost::shared_lock<boost::shared_mutex> lk(entry_mutex);//1
+	//		std::map<std::string, dns_entry>::const_iterator const it = 
+	//			entries.find(domain);
+	//		return (it == entries.end()) ? dns_entry() : it->second;
+	//	}
+
+	//	void update_or_add_entry(std::string const& domain, dns_entry const& dns_details)
+	//	{
+	//		std::lock_guard<boost::shared_mutex> lk(entry_mutex);//2
+	//		entries[domain] = dns_details
 	//	}
 	//};
 }
